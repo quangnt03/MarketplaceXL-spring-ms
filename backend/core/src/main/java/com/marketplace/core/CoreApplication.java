0ee61d381b1be641ae8b36bc912180f644b126e2
quangnt03 <ntq20803@gmail.com>
2026-07-30 16:33:30 +0700

    add and config logging for dev and prod environments

--- backend/core/src/main/java/com/marketplace/core/CoreApplication.java
+++ backend/core/src/main/java/com/marketplace/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.marketplace.core;
+package com.marketplace.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
